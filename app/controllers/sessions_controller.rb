347357690a5132738f613e7a5bbff74bf57128fd
Jared <jaredcmurphy@gmail.com>
2016-08-28 13:58:43 -0400

    successfully sign in with oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,7 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
-    api_user = ApiUser.find_by_provider_and_uid(auth["provider"], auth["uid"])
-      || ApiUser.create_with_omniauth(auth)
+    api_user = ApiUser.find_by_provider_and_uid(auth["provider"], auth["uid"]) || ApiUser.create_with_omniauth(auth)
 
     session[:api_user_id] = api_user.id
     redirect_to root_url, :notice => "Signed in!"
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do 
-  provider :github, ENV['OPEN_SOURCE_API_GITHUB_CLIENT_ID'], ENV['OPEN_SOURCE_API_GITHUB_CLIENT_SECRET']
+  provider :github, ENV['OPEN_SOURCE_API_GITHUB_CLIENT_ID'],
+  ENV['OPEN_SOURCE_API_GITHUB_CLIENT_SECRET']
 end
