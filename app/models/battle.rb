6a2663447b2f471803a529d186032f5641346fb8
Jared Murphy <jaredcmurphy@gmail.com>
2017-01-19 13:17:26 -0500

    update seed

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -1,6 +1,6 @@
 class Battle < ApplicationRecord
   validates :winner_score, :loser_score, :winner_id, :loser_id, presence: true
-  validates :winner_score, :loser_score, :winner_id, :loser_id, numericality: true 
+  validates :winner_score, :loser_score, :winner_id, :loser_id, numericality: true
 
   belongs_to :winner,
     class_name: "Player",
